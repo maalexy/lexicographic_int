f20690d9e2f17535de46268d81996f11251802ac
Alexy Marcell <alexy.marcell@hotmail.com>
2019-07-17 17:00:45 +0200

    Changed constants to optional parameters with default values.

--- example/lexicographic_int_example.dart
+++ example/lexicographic_int_example.dart
@@ -1,15 +1,15 @@
 import 'package:lexicographic_int/lexicographic_int.dart';
 
 main() {
-  final bigNumber1 = '61241643';
-  final bigNumber2 = '17568876414';
-  final encNumber1 = encode(bigNumber1);
-  final encNumber2 = encode(bigNumber2);
-  print(encNumber1);
-  print(encNumber2);
+  String bigNumber1 = '61241643';
+  String bigNumber2 = '17568876414';
+  String encNumber1 = encode(bigNumber1);
+  String encNumber2 = encode(bigNumber2);
+  print(encNumber1); // ^^861241643
+  print(encNumber2); // ^^^21117568876414
 
-  final encNumber3 = '^^3157';
-  final encNumber4 = '--743';
-  assert(decode(encNumber3) == '157');
-  assert(decode(encNumber4) == '-67');
+  String encNumber3 = '^^3157';
+  String encNumber4 = '--743';
+  print(decode(encNumber3)); // '157'
+  print(decode(encNumber4)); // '-67'
 }
